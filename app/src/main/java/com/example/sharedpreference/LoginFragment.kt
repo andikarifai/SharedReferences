1c6c6698734ffa56cb68800de19dab725b930c4c
BAGAS <anndptra658@gmail.com>
2023-04-11 11:38:43 +0700

    Menu Login

---
+++ app/src/main/java/com/example/sharedpreference/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.example.sharedpreference
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.sharedpreference.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSignin.setOnClickListener {
+            val user = binding.etUsername.text.toString()
+            val bundle = Bundle()
+            bundle.putString("User", user)
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+    }
+}
