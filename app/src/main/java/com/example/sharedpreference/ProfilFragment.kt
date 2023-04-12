3138e57b8511df3d6f4862d88217a8c44bd7f528
andikarifai <rifaiandika79@gmail.com>
2023-04-12 20:47:23 +0700

    menambahkan halaman profil

---
+++ app/src/main/java/com/example/sharedpreference/ProfilFragment.kt
@@ -0,0 +1,52 @@
+package com.example.sharedpreference
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sharedpreference.databinding.FragmentProfilBinding
+import com.example.sharedpreference.model.Film
+
+class ProfilFragment : Fragment() {
+
+    private var _binding: FragmentProfilBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfilBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Mengambil data dari Shared Preferences
+        val sharedPreferences = activity?.getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+        val name = sharedPreferences?.getString("NAME", "")
+
+        // Set data ke tampilan profil
+        binding.nameTextView.text = name
+
+        // Setup onClickListener untuk logout button
+        binding.logoutButton.setOnClickListener {
+            // Menghapus data dari Shared Preferences
+            sharedPreferences?.edit()?.clear()?.apply()
+
+            // Navigasi ke halaman login
+            val intent = Intent(activity, LoginFragment::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
